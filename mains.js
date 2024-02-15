fc83525dce3e04e2456ab95708efdb10203e803a
Talfaza <charifyahia4@gmail.com>
2024-02-15 14:20:54 +0100

    test

--- mains.js
+++ mains.js
@@ -1,58 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-      const lines = document.querySelectorAll('.line');
-      let index = 0;
-      
-      function displayLine() {
-        if (index < lines.length) {
-          lines[index].style.display = 'block';
-          index++;
-          setTimeout(displayLine, 500); 
-        } else {
-          // All lines are printed, display the input and button
-          const inputContainer = document.getElementById('inputContainer');
-          inputContainer.style.display = 'block';
-        }
-      }
-      
-      displayLine();
-    });
-    
-    document.addEventListener("DOMContentLoaded", function() {
-      const outputContainer = document.getElementById('outputContainer');
-      const textInput = document.getElementById('textInput');
-      
-      function displayMessage() {
-        const inputText = textInput.value;
-        const test = ["<br>", "Hello", "World"];
-        
-        outputContainer.innerHTML = '';
-    
-        if (inputText.toLowerCase() === "hello") {
-          // Display the content of the test array line by line with a timeout
-          let index = 0;
-          function displayLine() {
-            if (index < test.length) {
-              const itemElement = document.createElement('div');
-              itemElement.innerHTML = test[index];
-              outputContainer.appendChild(itemElement);
-              index++;
-              setTimeout(displayLine, 500); // ms
-            }
-          }
-          displayLine();
-        } else {
-          // Display a message if input doesn't match "hello"
-          const errorMessage = document.createElement('div');
-          errorMessage.textContent = "Input does not match 'hello'";
-          outputContainer.appendChild(errorMessage);
-        }
-      }
-    
-    
-      textInput.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-          displayMessage();
-        }
-      });
-    });
-    
+const asciiArt = [
+      "     '-.",
+      "        '-. _____    ",
+      " .-._      |     '.  ",
+      ":  ..      |      :  ",
+      "'-._+      |    .-' ",
+      " /  \\     .'i--i     ",
+      "/    \\ .-'_/____\\___ ",
+      "    .-'  :       "
+  ];
+
+const asciiArtElement = document.getElementById("ascii-art");
+
+function displayAsciiArtLineByLine(index) {
+    if (index < asciiArt.length) {
+        asciiArtElement.textContent += asciiArt[index] + "\n";
+        setTimeout(() => {
+            displayAsciiArtLineByLine(index + 1);
+        }, 100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    displayAsciiArtLineByLine(0);
+});
---
+++ modules.js
@@ -0,0 +1,4 @@
+export const test = ["<br>", "Hello", "World"];
+
+
+
